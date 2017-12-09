2034d345786ba383246e1b709679b7ff1e99c557
allen100309 <sunduo3195@.com>
2017-12-09 14:40:56 +0800

    修改了说明，以及plugins.js注释

--- src/config/plugins.js
+++ src/config/plugins.js
@@ -2,11 +2,12 @@
 * @Author: allen100309
 * @Date:   2017-12-09 11:51:29
 * @Last Modified by:   allen100309
-* @Last Modified time: 2017-12-09 12:01:56
+* @Last Modified time: 2017-12-09 14:39:47
 */
 import Vue from 'vue';
 //导入mint-ul插件
 import Mint from 'mint-ui';
 //注册mint插件
 Vue.use(Mint);
+//导入mint组件样式
 import 'mint-ui/lib/style.css';
