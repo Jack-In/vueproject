4275da3845d09c65b42fad32f7afec20e2cdfd39
allen100309 <sunduo3195@.com>
2017-12-10 02:23:45 +0800

    去除合并后的文件

--- src/config/plugins.js
+++ src/config/plugins.js
@@ -1,9 +1,9 @@
 /*
 * @Author: allen100309
 * @Date:   2017-12-09 11:51:29
-* @Last Modified by:   allen100309
+* @Last Modified by:   sunduo
 <<<<<<< HEAD
-* @Last Modified time: 2017-12-09 14:39:47
+* @Last Modified time: 2017-12-10 02:21:44
 =======
 * @Last Modified time: 2017-12-09 21:16:42
 >>>>>>> 0b0ee6463bb1e9b141418dc3f652765c00553afb
@@ -15,8 +15,4 @@ import Mint from 'mint-ui';
 //注册mint插件
 Vue.use(Mint);
 //导入mint组件样式
-<<<<<<< HEAD
 import 'mint-ui/lib/style.css';
-=======
-import 'mint-ui/lib/style.css';
->>>>>>> 0b0ee6463bb1e9b141418dc3f652765c00553afb
